42e87c55b45fc268205f1f911ac9a2896a7d46f1
董宇坤 <11336028+ustcflask@user.noreply.gitee.com>
2023-08-26 20:09:42 +0800

    update sketch.py

--- exp6/preprocess/dataloader.py
+++ exp6/preprocess/dataloader.py
@@ -8,7 +8,6 @@ from .base import AbstractDataset
 class Dataset(AbstractDataset):
 
     def __init__(self, df, has_label=True, **kwargs):
-        df = normalize(df)
         self.data = to_table(df)
         self.splitted = False
         self.has_label = has_label
@@ -63,18 +62,50 @@ class DataLoader:
         return self.iter_fn(self.data, **params, **self.params)
 
 
-# 这个应该是咱们要写的最关键部分
 def get_timestep_data_iter(*data, batch_size, time_step, device, **kwargs):
-    '''
-    关于迭代器生成器的一些要求：
-    * 参数列表形如 get_iter(*data, batch_size, time_step, device, **kwargs)
-      其中 *data 处会传入数据集构成的元组，格式和 Dataset 构建时转成 torch 张量时的存储格式相同
-      相当于data就是to_table这个函数的输出，加星号就是解包（个人理解）
-    * 生成的迭代器的格式需要对接 models.structure.BasicExcutor.forward 的输入以及 train.loss.CombinedLoss 的输入
-      具体来说在 train 中希望通过下面的代码计算由迭代器生成的小批次数据 X 中损失
-      X, y = X[: -1], X[-1]
-      l = loss(net(*X), *y)
-    * 每个批次的数据需要在时间步上对齐，例如 features[1, t, :] 和 features[3, t, :] 需要在同一天
-    * 如果没有足够的数据，返回的小批次的数据的 batch_size 和 time_step 可以比设定值小
-    '''
-    return 0
+    has_label = len(data) == 6
+    if has_label:
+        T, I, X, X_selected, msk, label = data
+    else:
+        T, I, X, X_selected, msk = data
+
+    offset = random.randint(0, time_step - 1)
+    total_timesteps = T.shape[0]
+    total_stocks = T.shape[1]
+
+    for t in range(offset, total_timesteps - time_step - 2, time_step):
+        for start_stock in range(0, total_stocks, batch_size):
+            end_stock = start_stock + batch_size
+
+            batch_T = T[t:t+time_step, start_stock:min(end_stock, total_stocks)]
+            batch_I = I[t:t+time_step, start_stock:min(end_stock, total_stocks)]
+            batch_X = X[t:t+time_step, start_stock:min(end_stock, total_stocks)]
+            batch_X_selected = X_selected[t:t+time_step, start_stock:min(end_stock, total_stocks)]
+            batch_msk = msk[t:t+time_step, start_stock:min(end_stock, total_stocks)]
+
+            batch_T = batch_T.permute(1, 0) if len(batch_T.shape) == 2 else batch_T.permute(1, 0, 2)
+            batch_I = batch_I.permute(1, 0) if len(batch_I.shape) == 2 else batch_I.permute(1, 0, 2)
+            batch_X = batch_X.permute(1, 0) if len(batch_X.shape) == 2 else batch_X.permute(1, 0, 2)
+            batch_X_selected = batch_X_selected.permute(1, 0) if len(batch_X_selected.shape) == 2 else batch_X_selected.permute(1, 0, 2)
+            batch_msk = batch_msk.permute(1, 0) if len(batch_msk.shape) == 2 else batch_msk.permute(1, 0, 2)
+
+            if has_label:
+                batch_label = label[t:t+time_step, start_stock:min(end_stock, total_stocks)]
+                batch_label = batch_label.permute(1, 0) if len(batch_label.shape) == 2 else batch_label.permute(1, 0, 2)
+
+                y = batch_label
+                y_msk = batch_msk
+                X_day_1 = X_selected[t+1:t+time_step+1, start_stock:min(end_stock, total_stocks)]
+                msk_day_1 = msk[t+1:t+time_step+1, start_stock:min(end_stock, total_stocks)]
+                X_day_2 = X_selected[t+2:t+time_step+2, start_stock:min(end_stock, total_stocks)]
+                msk_day_2 = msk[t+2:t+time_step+2, start_stock:min(end_stock, total_stocks)]
+
+                X_day_1 = X_day_1.permute(1, 0) if len(X_day_1.shape) == 2 else X_day_1.permute(1, 0, 2)
+                msk_day_1 = msk_day_1.permute(1, 0) if len(msk_day_1.shape) == 2 else msk_day_1.permute(1, 0, 2)
+                X_day_2 = X_day_2.permute(1, 0) if len(X_day_2.shape) == 2 else X_day_2.permute(1, 0, 2)
+                msk_day_2 = msk_day_2.permute(1, 0) if len(msk_day_2.shape) == 2 else msk_day_2.permute(1, 0, 2)
+
+                y_tuple = (y, y_msk, X_day_1, msk_day_1, X_day_2, msk_day_2)
+                yield batch_T, batch_I, batch_X, batch_X_selected, batch_msk, y_tuple
+            else:
+                yield batch_T, batch_I, batch_X, batch_X_selected, batch_msk
